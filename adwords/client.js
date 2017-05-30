2ff08657f4f7b5b6cface8c9ec3568741dfa99b8
Luke van der Palen <lukevdpalen@gmail.com>
2017-05-30 17:59:09 +0200

    Added support for Service Account authentication

--- adwords/client.js
+++ adwords/client.js
@@ -8,28 +8,21 @@ const _ = require('lodash');
 const AdwordsServiceDescriptors = require('../services');
 const AdwordsService = require('./service');
 const AdwordsConstants = require('./constants');
-const AdwordsAuth = require('./auth');
 
-class AdwordsUser {
+class AdwordsClient {
 
     /**
      * @inheritDoc
      */
-    constructor(obj) {
+    constructor(oAuth2Client, obj) {
         this.credentials = _.extend({
             developerToken: '',
             userAgent: 'node-adwords',
             clientCustomerId: '',
-            client_id: '',
-            client_secret: '',
-            refresh_token: '',//@todo implement refesh token instead of access token
-            access_token: '',
         }, obj);
 
-        const auth = new AdwordsAuth( this.credentials );
-        this.oAuth2Client = auth.oAuth2Client;
+        this.oAuth2Client = oAuth2Client;
     }
-
 
     /**
      * Returns an Api Service Endpoint
@@ -75,4 +68,4 @@ class AdwordsUser {
 
 }
 
-module.exports = AdwordsUser;
+module.exports = AdwordsClient;
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     AdwordsAuth: require('./adwords/auth'),
+    AdwordsClient: require('./adwords/client'),
     AdwordsUser: require('./adwords/user'),
     AdwordsConstants: require('./adwords/constants'),
     AdwordsReport: require('./adwords/report')
