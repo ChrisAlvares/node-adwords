8db78d519baf3bb327c2e2831267d637b92eaca4
Chris Alvares <chris.alvares@pmg.com>
2017-06-19 10:03:33 -0500

    updates apis to 201705

--- test/adwords/services/adwordsuser-config.js
+++ test/adwords/services/adwordsuser-config.js
@@ -18,7 +18,7 @@ if (developerToken && refreshToken && clientId && clientSecret && clientCustomer
         client_id: clientId,
         client_secret: clientSecret,
         refresh_token: refreshToken,
-        version: 'v201702'
+        version: 'v201705'
     };
 }
 
