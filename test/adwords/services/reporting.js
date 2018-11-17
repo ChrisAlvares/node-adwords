adfd19294d121ce2277e7af824a45a218016129e
Rich Cain <rich.cain@varsitytutors.com>
2018-11-16 16:08:14 -0800

    Adding stream capability

--- test/adwords/services/reporting.js
+++ test/adwords/services/reporting.js
@@ -4,10 +4,100 @@
  */
 const _ = require('lodash');
 const AdwordsReport = require('../../../index').AdwordsReport;
+const sinon = require('sinon');
+const https = require('https');
+const http = require('http');
+const os = require('os');
+const path = require ('path');
+const assert = require('assert');
+const MockReq = require('mock-req');
+const MockRes = require('mock-res');
 
 describe('ReportService', function() {
+    // let config = require('./adwordsuser-config');
+    let config;
 
-    let config = require('./adwordsuser-config');
+    describe('streamReport', function() {
+        let post;
+
+        beforeEach(() => {
+            post = sinon.stub(https, 'request');
+        });
+
+        afterEach(() => {
+            https.request.restore();
+        });
+
+        it('streamReport success', function(done) {
+            let report = new AdwordsReport({});
+
+            sinon.stub(report, 'getHeaders').yields(null, {});
+            sinon.stub(report, 'buildParams').returns({});
+
+            let responseBody = "Header1,Header2,Header3\n1,2,3\n";
+
+            let response = new MockRes();
+
+            let request = new MockReq({
+                method: 'POST'
+            });
+
+            let dataSpy = sinon.spy();
+            response.on('data', dataSpy);
+
+            post.yields(response).returns(request);
+
+            let callback = (res) => {
+              assert.equal(res, response);
+              done();
+            };
+
+            report.streamReport('v201809', { header: 'test' }, callback);
+        });
+
+        it('should error out on getHeaders', function(done){
+            let report = new AdwordsReport({});
+
+            sinon.stub(report, 'getHeaders').yields({ error: 'error'}, {});
+
+            let callback = function(_error, status) {
+                if (!status) {
+                    return done();
+                }
+
+                done(new Error('Should have errored with bad access token'));
+            };
+
+            report.streamReport('v201809', { header: 'test' }, callback);
+        });
+
+        it('should error out on post', function(done) {
+            let report = new AdwordsReport({});
+
+            sinon.stub(report, 'getHeaders').yields(null, {});
+            sinon.stub(report, 'buildParams').returns({});
+
+            let callback = function(_error, status) {
+                if (!status) {
+                    return done();
+                }
+
+                done(new Error('Should have errored'));
+            };
+
+            let response = new MockRes();
+            response.statusCode = 400;
+
+            let request = new MockReq({
+                method: 'POST'
+            });
+
+            post.yields(response).returns(request);
+
+            report.streamReport('v201809', { header: 'test' }, callback);
+        });
+    });
+
     if (!config) {
         return console.log('Adwords User not configured, skipping ReportService Service tests');
     }
@@ -117,5 +207,6 @@ describe('ReportService', function() {
             }, done);
         });
     });
+
 
 });
